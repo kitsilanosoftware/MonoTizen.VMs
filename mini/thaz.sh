5ffde35274a612192f6888fbe24df812075349d0
Damien Diederen <dd@crosstwine.com>
2014-06-26 19:15:56 +0200

    Get rid of Git-Annexed images and startup scripts

--- mini/thaz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE="$(dirname "$0")"
-
-exec qemu-system-i386 -m 1G -enable-kvm                         \
-    -kernel "$BASE/../sdk-2.2/bzImage.x86"                      \
-    -drive "file=$BASE/thaz.qcow2,if=virtio,index=1" -boot c    \
-    -net nic                                                    \
-    -net user,hostfwd=tcp::2223-:22
