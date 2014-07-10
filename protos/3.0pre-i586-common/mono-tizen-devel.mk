7d06898f24b04f26f85fd8df4bffdd75f88fdbb7
Damien Diederen <dd@crosstwine.com>
2014-07-11 01:05:28 +0200

    protos/3.0pre-i586-common: Preload RPMs in VM

--- protos/3.0pre-i586-common/mono-tizen-devel.mk
+++ protos/3.0pre-i586-common/mono-tizen-devel.mk
@@ -16,12 +16,39 @@
 # along with MonoTizen.  If not, see <http://www.gnu.org/licenses/>.
 
 TIZEN_VM__3_0pre_i586_PREFIX_URL = \
-	http://download.tizen.org/releases/daily/tizen/common/tizen_20140704.1/
+	http://download.tizen.org/releases/daily/tizen/common/tizen_20140704.1
 
 TIZEN_VM__mono_tizen_devel_REPO_URL = \
 	$(TIZEN_VM__3_0pre_i586_PREFIX_URL)/repos/emulator32-wayland/packages
 
-TIZEN_VM__mono_tizen_devel_RPMS =
+TIZEN_VM__mono_tizen_devel_RPMS =			\
+	i686/binutils-2.23.1-5.1.i686.rpm		\
+	i686/bison-2.6.2-5.1.i686.rpm			\
+	i686/cpp-4.8-4.1.i686.rpm			\
+	i686/cpp48-4.8.2-5.1.i686.rpm			\
+	i686/fdupes-1.40-4.1.i686.rpm			\
+	i686/gcc-4.8-4.1.i686.rpm			\
+	i686/gcc-c++-4.8-4.1.i686.rpm			\
+	i686/gcc48-4.8.2-5.1.i686.rpm			\
+	i686/gcc48-c++-4.8.2-5.1.i686.rpm		\
+	i686/gettext-runtime-0.18.3.2-4.1.i686.rpm	\
+	i686/gettext-tools-0.18.3.2-4.1.i686.rpm	\
+	i686/glibc-devel-2.18-4.3.i686.rpm		\
+	i686/libasan-4.8.2-5.1.i686.rpm			\
+	i686/libatomic-4.8.2-5.1.i686.rpm		\
+	i686/libgomp-4.8.2-5.1.i686.rpm			\
+	i686/libitm-4.8.2-5.1.i686.rpm			\
+	i686/libmpc-1.0-4.1.i686.rpm			\
+	i686/libmpfr-3.1.1-4.1.i686.rpm			\
+	i686/libstdc++48-devel-4.8.2-5.1.i686.rpm	\
+	i686/libtool-2.4.2-4.1.i686.rpm			\
+	i686/m4-1.4.16-4.1.i686.rpm			\
+	i686/make-3.82-4.1.i686.rpm			\
+	i686/rpm-build-4.11.0.1-13.1.i686.rpm		\
+	i686/zlib-devel-1.2.7-4.1.i686.rpm		\
+	noarch/autoconf-2.69-5.1.noarch.rpm		\
+	noarch/automake-1.12.1-5.1.noarch.rpm		\
+	noarch/linux-glibc-devel-3.10-4.1.noarch.rpm
 
 # KLUDGE: This has nothing to do here; TODO: modularize.
 TIZEN_VM__mono_tizen_devel_RPMS +=		\
